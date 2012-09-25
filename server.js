5ecc1a2526b5495f2fab428342d23e03f25aefcc
Jaz <jazlalli@hotmail.com>
2012-09-25 18:22:27 +0100

    loads of changes (mainly adding data plotting on gmaps)

--- server.js
+++ server.js
@@ -2,39 +2,43 @@ var http = require('http');
 var fs = require('fs');
 var amqp = require('amqp');
 
-var connection = amqp.createConnection(
-  {url: "<your amqp url>"});
+var connection = amqp.createConnection({
+  url: "<your amqp url>"
+});
 
-var clients = new Array();
-var todaysData = new Array();
-  
+var AppData = {
+  clients : [],
+  visitsToday : []
+};
+
 http.createServer(function (request, response) {
   // determine whether it is the eventstream request
-  if(request.headers.accept && request.headers.accept == 'text/event-stream') {
+  if (request.headers.accept && request.headers.accept === 'text/event-stream') {
     response.writeHead(200, {
       'Content-Type': 'text/event-stream',
       'Cache-Control': 'no-cache',
       'Connection': 'keep-alive',
       'Accept' : '*/*',
       'Access-Control-Allow-Origin' : '*'
-    });  
-    
-    clients.push(response);
+    });
+
+    AppData.clients.push(response);
     console.log('New connection');
 
-    for(var i=0; i<clients.length; i++){
-      clients[i].write('data: ' + JSON.stringify(todaysData) + '\n\n');
+    for (var i=0; i<AppData.clients.length; i++){
+      AppData.clients[i].write('data: ' + JSON.stringify(AppData.visitsToday) + '\n\n');
     }
-  } else {
+  } 
+  else {
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.write(fs.readFileSync(__dirname + '/index.html'));
     response.end();
   }
   
-  request.on('close', function(){
+  request.on('close', function () {
 		console.log('connection closing');
-    if(clients.indexOf(response) >= 0) {
-      clients.pop(response);
+    if (AppData.clients.indexOf(response) >= 0) {
+      AppData.clients.pop(response);
     }
 	});  
 }).listen(1337);
@@ -43,12 +47,10 @@ console.log('Server running on port 1337');
 
 connection.on('ready', function () {
   // connect to existing exchange
-  connection.exchange("Visits Exchange", options={passive: 'true'}, function(exchange) {
-
+  connection.exchange("Visits Exchange", options={passive: 'true'}, function (exchange) {
     console.log('Connected to: ' + exchange.name);
-
     // define queue
-    connection.queue("Node Visits Queue", function(queue){
+    connection.queue("Node Visits Queue", function (queue) {
       
       console.log('Created queue: ' + queue.name);
       queue.bind(exchange, '#'); 
@@ -60,18 +62,19 @@ connection.on('ready', function () {
         var payload = JSON.parse(encoded_payload);
         var visitData = parse_message(payload);
         
-        init_data();
-        todaysData.push(visitData);
+        init_dataObj();
+        AppData.visitsToday.push(visitData);
         
-        for(var i=0; i<clients.length; i++){
-          clients[i].write('data: ' + JSON.stringify(visitData) + '\n\n');
+        for (var i=0; i<AppData.clients.length; i++){
+          AppData.clients[i].write('data: ' + JSON.stringify([visitData]) + '\n\n');
         }
       })
     });
   });
 });
 
-function parse_message(payload) {
+// make a nice object
+function parse_message (payload) {
   var urlParam = payload.QUERY_STRING.split('&')[1];
   var url = urlParam.substr(4, urlParam.length);
   
@@ -91,18 +94,38 @@ function parse_message(payload) {
   };
 }
 
-function init_data(){
+// keep using global object, or clean it out and start again
+function init_dataObj () {
   var now = new Date();
   
-  var lastMessageTime;
-  if(todaysData.length > 1){
-    lastMessageTime = new Date(todaysData.length-1).createtime;
+  var lastMessageDate;
+
+  if (AppData.visitsToday.length > 0){
+    lastMessageDate = parse_date(AppData.visitsToday[AppData.visitsToday.length-1].createtime);
   }
 
-  if(lastMessageTime){
-    console.log(lastMessageTime);
-    if(lastMessageTime.getDay() > now.getDay() || (now.getDay() === 0 && lastMessageTime.getDay() === 6)) {
-      todaysData.length = 0;
+  if (lastMessageDate){
+    if (lastMessageDate.getDate() > now.getDate() || (now.getDate() === 1 && lastMessageDate.getDate() > 1)) {
+      AppData.visitsToday.length = 0;
     }
   }
+}
+
+// make a nice date object from string dd/mm/YYYY HH:MM:ss
+function parse_date (lastMessageTimeStr) {
+  var formattedDateStr = lastMessageTimeStr.split(' ')[0];
+
+  var dayStr = formattedDateStr.split('-')[0],
+      monthStr = formattedDateStr.split('-')[1],
+      yearStr = formattedDateStr.split('-')[2];
+
+  if (monthStr.substr(0, 1) === '0'){
+    monthStr = monthStr.substr(1, 1);
+  }
+
+  var day = parseInt(dayStr),
+      month = parseInt(monthStr),
+      year = parseInt(yearStr);
+
+  return new Date(year, month-1, day);
 }
